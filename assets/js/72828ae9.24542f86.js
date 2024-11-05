6d213991f371697382be0686b03cbb630c515eca
Wida Putri Kinasih <158005362+widaputri@users.noreply.github.com>
2024-11-05 21:05:04 +0700

    Deploy website - based on 8173cd4a8d2de420b6a319dc5a9036ec0b9ba22f

---
+++ assets/js/72828ae9.24542f86.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_242_saurus=self.webpackChunk_242_saurus||[]).push([[705],{5127:s=>{s.exports=JSON.parse('{"metadata":{"permalink":"/242saurus/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":5,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
